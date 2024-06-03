43f3ca280b7f6a74ed5f271879ebf5edc84fc959
noricha-vr <noricha.vr@gmail.com>
2024-06-03 21:38:40 +0900

    Change allow hosts

--- app/website/settings.py
+++ app/website/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
-
+ALLOWED_HOSTS = ['vrc-ta-hub.com',os.environ.get('HTTP_HOST')]
 
 # Application definition
 
