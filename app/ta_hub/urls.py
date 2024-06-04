5c41d10027272e04f88bd0458b956906768286e1
noricha-vr <noricha.vr@gmail.com>
2024-06-04 15:26:08 +0900

    Fix typo

--- app/ta_hub/urls.py
+++ app/ta_hub/urls.py
@@ -25,5 +25,5 @@ from django.views.generic import TemplateView
 app_name = 'ta_hub'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='ta_hub/index.html'), name='index'),
-    path('event_list/', TemplateView.as_view(template_name='ta_hub/envent_list.html'), name='event_list'),
+    path('event_list/', TemplateView.as_view(template_name='ta_hub/event_list.html'), name='event_list'),
 ]
