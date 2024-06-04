4998af28939d622a624fdc9b49f145811e6aefde
noricha-vr <noricha.vr@gmail.com>
2024-06-04 12:34:12 +0900

    Add update view

--- app/event/urls.py
+++ app/event/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import EventListView, EventDetailView
+from .views import EventListView, EventDetailView, calendar_view
 
 app_name = 'event'
 urlpatterns = [
     path('list/', EventListView.as_view(), name='list'),
     path('<int:pk>/', EventDetailView.as_view(), name='detail'),
+    path('update/', calendar_view, name='update'),
 ]
