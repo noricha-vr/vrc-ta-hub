86efcf1bef6794bc6e0fb303b384d2e08084f487
noricha-vr <noricha.vr@gmail.com>
2024-06-05 11:36:54 +0900

    Rename: community fields Add: updated_at

--- app/community/models.py
+++ app/community/models.py
@@ -28,16 +28,17 @@ TAGS = (
 
 
 class Community(models.Model):
-    name = models.CharField('イベント名', max_length=100, db_index=True)
-    start_from = models.DateField('イベント開始日', default=None, blank=True, null=True)
-    end_at = models.DateField('イベント終了日', default=None, blank=True, null=True)
-    start_time = models.TimeField('開始時刻', default='22:00')
+    name = models.CharField('集会名', max_length=100, db_index=True)
+    created_at = models.DateField('開始日', default=timezone.now, blank=True, null=True, db_index=True)
+    updated_at = models.DateField('更新日', auto_now=True, db_index=True)
+    end_at = models.DateField('終了日', default=None, blank=True, null=True)
+    start_time = models.TimeField('開始時刻', default='22:00', db_index=True)
     duration = models.IntegerField('開催時間', default=60, help_text='単位は分')
     weekday = models.CharField('曜日', max_length=5, choices=WEEKDAY_CHOICES)
     frequency = models.CharField('開催周期', max_length=100)
     organizers = models.CharField('主催・副主催', max_length=200)
     group_url = models.URLField('VRChatグループURL', blank=True)
-    organizer_url = models.URLField('VRChat主催プロフィールURL', blank=True)
+    organizer_url = models.URLField('主催プロフィールURL', blank=True)
     discord = models.URLField('Discord', blank=True)
     twitter_hashtag = models.CharField('Twitterハッシュタグ', max_length=100, blank=True)
     poster_image = models.ImageField('ポスター', upload_to='poster/', blank=True)
