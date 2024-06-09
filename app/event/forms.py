2f02543890c04629c3f73091d560317260d06049
noricha-vr <noricha.vr@gmail.com>
2024-06-09 16:54:44 +0900

    Apply bootstrap on form

--- app/event/forms.py
+++ app/event/forms.py
@@ -1,18 +1,21 @@
-# forms.py
-
-from django import forms
-from community.models import WEEKDAY_CHOICES, TAGS
-
-# forms.py
-
-from django import forms
-from community.models import WEEKDAY_CHOICES, TAGS
-
 from django import forms
 from community.models import WEEKDAY_CHOICES
 
 
 class EventSearchForm(forms.Form):
-    name = forms.CharField(label='集会名', required=False)
-    weekday = forms.MultipleChoiceField(label='曜日', choices=WEEKDAY_CHOICES, required=False,
-                                        widget=forms.CheckboxSelectMultiple)
+    name = forms.CharField(
+        label='集会名',
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': '集会名を入力',
+        })
+    )
+    weekday = forms.MultipleChoiceField(
+        label='曜日',
+        choices=WEEKDAY_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'form-check-input',
+        })
+    )
