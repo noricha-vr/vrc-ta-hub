b3bf1eaa8c57d3ca418a16531e06ac85d874b98a
noricha-vr <noricha.vr@gmail.com>
2024-06-14 10:47:16 +0900

    Fix: favicon load error

--- app/sitemap/urls.py
+++ app/sitemap/urls.py
@@ -19,24 +19,10 @@ from .views import SitemapView
 
 app_name = 'sitemap'
 
-# Redirect settings
-favicon_view = RedirectView.as_view(
-    url='https://data.vrc-ta-hub.com/images%2Fsite%2Ffavicon.ico',
-    permanent=True)
-icon120_view = RedirectView.as_view(
-    url='https://data.vrc-ta-hub.com/images%2Fsite%2Fapple-touch-icon.png',
-    permanent=True)
-icon160_view = RedirectView.as_view(
-    url='https://data.vrc-ta-hub.com/images%2Fsite%2Fapple-touch-icon.png',
-    permanent=True)
 urlpatterns = [
     # path('sitemap.xml', SitemapListView.as_view(), name='sitemap_list'),
-    path('favicon.ico', favicon_view),
     path('robots.txt', TemplateView.as_view(template_name='sitemap/robots.txt')),
-    path('apple-touch-icon.png', icon120_view),
-    path('apple-touch-icon-precomposed.png', icon120_view),
-    path('apple-touch-icon-152x152.png', icon160_view),
-    path('apple-touch-icon-152x152-precomposed.png', icon160_view),
+
     path('sitemap.xml', SitemapView.as_view(), name='sitemap'),
     path('sitemaps.xml', RedirectView.as_view(url='/sitemap.xml', permanent=True)),
 ]
--- app/ta_hub/views.py
+++ app/ta_hub/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.static import serve
 
 
 class IndexView(TemplateView):
     template_name = 'ta_hub/index.html'
+
+
+def favicon_view(request):
+    return serve(request, 'favicon.ico', document_root='site')
+
+
+def apple_touch_icon_view(request):
+    return serve(request, 'apple-touch-icon.png', document_root='site')
