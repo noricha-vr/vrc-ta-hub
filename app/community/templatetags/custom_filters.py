d9f2f3a8d38d7c61cb231b6785e3496c1b8aed2f
noricha-vr <noricha.vr@gmail.com>
2024-07-03 02:44:47 +0900

    Create weekday_abbr

--- app/community/templatetags/custom_filters.py
+++ app/community/templatetags/custom_filters.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def weekday_abbr(weekday):
+    weekday_dict = {
+        'Sun': '日', 'Mon': '月', 'Tue': '火', 'Wed': '水',
+        'Thu': '木', 'Fri': '金', 'Sat': '土', 'Other': '他'
+    }
+    return weekday_dict.get(weekday, weekday)
