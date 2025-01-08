2fad8fc2f5de326624b3551c733e4a73d9902cf0
noricha-vr <noricha.vr@gmail.com>
2025-01-08 21:30:42 +0900

    Delete init help_text

--- app/account/forms.py
+++ app/account/forms.py
@@ -107,10 +107,6 @@ class CustomUserCreationForm(UserCreationForm):
             },
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['email'].required = True
-        self.fields['email'].help_text = '必須。有効なメールアドレスを入力してください。'
 
     def save(self, commit=True):
         user = super().save(commit=False)
