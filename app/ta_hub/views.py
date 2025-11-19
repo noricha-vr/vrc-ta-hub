62a16323b0a81f81a306ad4fab54a2450f9cd7de
noricha-vr <noricha.vr@gmail.com>
2025-11-20 00:33:06 +0900

    feat: Vket2025 Winter告知バナーを実装
    - 2025年11月20日から12月22日まで自動表示
    - Twitterハッシュタグリンク（#Vket技術学術WEEK、#Vket2025Winter）を追加
    - 特別イベント開催中ボタンでVket期間内のLTへスムーズスクロール
    - Winter仕様の青系グラデーションデザインに変更

--- app/ta_hub/views.py
+++ app/ta_hub/views.py
@@ -26,9 +26,9 @@ class IndexView(TemplateView):
 
         # Vketコラボ告知の表示判定
         current_datetime = timezone.now()
-        vket_publish_datetime = timezone.datetime(2025, 6, 30, 12, 0, tzinfo=timezone.get_current_timezone())
-        vket_start_datetime = timezone.datetime(2025, 7, 12, 0, 0, tzinfo=timezone.get_current_timezone())
-        vket_end_datetime = timezone.datetime(2025, 7, 28, 0, 0, tzinfo=timezone.get_current_timezone())
+        vket_publish_datetime = timezone.datetime(2025, 11, 20, 12, 0, tzinfo=timezone.get_current_timezone())
+        vket_start_datetime = timezone.datetime(2025, 12, 6, 0, 0, tzinfo=timezone.get_current_timezone())
+        vket_end_datetime = timezone.datetime(2025, 12, 22, 0, 0, tzinfo=timezone.get_current_timezone())
         context['show_vket_notice'] = vket_publish_datetime <= current_datetime < vket_end_datetime
         context['vket_start_date'] = vket_start_datetime.date()
         context['vket_end_date'] = vket_end_datetime.date()
