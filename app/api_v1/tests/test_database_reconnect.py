6410d0f17ec4753b54253d8dab7664939c01f26c
noricha-vr <noricha.vr@gmail.com>
2026-05-07 09:29:33 +0900

    fix: DB接続断時のAPI応答を安定化

--- app/api_v1/tests/test_database_reconnect.py
+++ app/api_v1/tests/test_database_reconnect.py
@@ -17,12 +17,36 @@ class TransientFailureListViewSet(viewsets.ViewSet):
         return Response({'attempts': self.attempts})
 
 
+class PersistentConnectFailureListViewSet(viewsets.ViewSet):
+    def list(self, request):
+        raise OperationalError(2002, "Can't connect to server on 'mysql.example.test' (115)")
+
+
+class HandshakeFailureListViewSet(viewsets.ViewSet):
+    def list(self, request):
+        raise OperationalError(
+            2013,
+            "Lost connection to server at 'handshake: reading initial communication packet'",
+        )
+
+
 class NonRetryableFailureListViewSet(viewsets.ViewSet):
     def list(self, request):
         raise OperationalError(1064, 'SQL syntax error')
 
 
 class RetryingListViewSet(DatabaseReconnectListMixin, TransientFailureListViewSet):
+    pass
+
+
+class PersistentConnectFailureRetryingListViewSet(
+    DatabaseReconnectListMixin,
+    PersistentConnectFailureListViewSet,
+):
+    pass
+
+
+class HandshakeFailureRetryingListViewSet(DatabaseReconnectListMixin, HandshakeFailureListViewSet):
     pass
 
 
@@ -42,6 +66,24 @@ class DatabaseReconnectListMixinTest(SimpleTestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {'attempts': 2})
+
+    def test_list_returns_503_when_mysql_connect_failure_persists(self):
+        view = PersistentConnectFailureRetryingListViewSet.as_view({'get': 'list'})
+        request = self.factory.get('/api/v1/community/')
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, 503)
+        self.assertEqual(response.data, {'detail': 'Database temporarily unavailable.'})
+
+    def test_list_returns_503_when_mysql_handshake_failure_persists(self):
+        view = HandshakeFailureRetryingListViewSet.as_view({'get': 'list'})
+        request = self.factory.get('/api/v1/community/')
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, 503)
+        self.assertEqual(response.data, {'detail': 'Database temporarily unavailable.'})
 
     def test_list_does_not_retry_non_disconnect_operational_error(self):
         view = NonRetryableListViewSet.as_view({'get': 'list'})
